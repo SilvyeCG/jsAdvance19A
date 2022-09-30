2020dad53c664f6a0583ce5bde7748d74c92424d
Silvia <scortesg9527@gmail.com>
2022-09-29 20:35:54 -0500

    feat: Create author

--- 08-apiCRUD/crudAuthors.js
+++ 08-apiCRUD/crudAuthors.js
@@ -10,7 +10,11 @@ const request = require('request');
 // 2. Endpoint
 const URI = 'https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/'
 
-// Get authors list
+/**
+ * Get authors list:
+ * It makes a request to the URI, and if the response is 200, it parses the body and logs it to the
+ * console.
+ */
 const getAuthors = () =>{
     request.get(URI, (error, response, body) =>{
         //validate petition
@@ -23,8 +27,12 @@ const getAuthors = () =>{
     });
 }
 
-//GET AUTHOR BY ID
-
+/**
+ * GET AUTHOR BY ID: 
+ * It makes a request to the API, and if the response is 200, it parses the body and prints it to the
+ * console.
+ * @param idAuthor - The id of the author you want to get.
+ */
 const getAuthorById  = (idAuthor) => {
     request.get(URI+idAuthor+'/',(error, response, body) =>{
         //validate petition
@@ -37,8 +45,34 @@ const getAuthorById  = (idAuthor) => {
     });
 }
 
+/**
+ * POST/CREATE new author: A JSON Format will be needed
+ * It takes a JSON object as a parameter, and then it makes a POST request to the endpoint URI, and
+ * then it logs the response to the console.
+ * @param jsonData - 
+ */
+const createAuthor = (jsonData) => {
+    const objConfig = {
+        url: URI, //Endpoint
+        form: jsonData //new Author DATA in JSON Format
+    }
 
+    request.post(objConfig, (error, response, body) => {
+        //validate petition
+        if(response.statusCode === 201){
+            const createAuthor = JSON.parse(body)
+            console.log('Author created successfully' + createAuthor)
+        }else{
+            console.log(response.statusCode, response.statusMessage);
+        }
+    })
+}
+
+
+
+/* Exporting the functions to be used in another file. */
 module.exports = {
     getAuthors,
-    getAuthorById
+    getAuthorById,
+    createAuthor
 }
--- 08-apiCRUD/main.js
+++ 08-apiCRUD/main.js
@@ -7,4 +7,18 @@ const goodReadsCrud = require('./crudAuthors')
 //goodReadsCrud.getAuthors();
 
 //get author by ID
-goodReadsCrud.getAuthorById(14710);
+//goodReadsCrud.getAuthorById(14890);
+
+// Create author, a JSON must be the param
+
+const jsonSend = {
+	name: "Jennette",
+	last_name: "McCurdy",
+	nacionalidad: "MX",
+	biography: "I'm glad my mom die",
+	gender: "F",
+	age: 64,
+	is_alive: true
+}
+//IDs 14899 => Maria, 14890 => Silvia ,14901 => Jennette
+goodReadsCrud.createAuthor(jsonSend)
